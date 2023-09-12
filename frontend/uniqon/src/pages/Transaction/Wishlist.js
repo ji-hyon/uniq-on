82bd0afaf979ec0bf0f8eb164e2ce46f30769468
방상제 <jebread@JeBread.local>
2023-09-12 21:09:54 +0900

    feat:<Transaction>판매 & 위시리스트 samplecode 작성

--- frontend/uniqon/src/pages/Transaction/Wishlist.js
+++ frontend/uniqon/src/pages/Transaction/Wishlist.js
@@ -1,10 +1,75 @@
+import React from "react";
+import axios from "axios";
+import { Button } from "@material-tailwind/react";
+
+import { useEffect } from "react";
+
 export function Wishlist() {
+
+  const URL = "http://localhost:8080"
+
+  // useEffect(() => {
+  //   getDefaultWishlist();
+  // }, []);
+
+
+  // function getDefaultWishlist() {
+      
+  //     axios
+  //       .get("/api/wishlist/1")
+  //       .then((res) => {
+  //         console.log(res.data)
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   }
+
+  function getWishlist() {
+      
+      axios
+        .get(URL + "/api/wishlist/")
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+
+  function addWishlist() {
+      
+      axios
+        .post(URL + "/api/wishlist/add/1")
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+
+  function deleteWishlist() {
+      
+      axios
+        .delete(URL + "/api/wishlist/1")
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }  
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Wishlist
         </p>
+        <Button onClick={getWishlist}>getWishlist</Button>
+        <Button onClick={addWishlist}>addWishlist</Button>
+        <Button onClick={deleteWishlist}>deleteWishlist</Button>
       </header>
     </div>
   );
