275a4be98650515a738154dc5e2a8ea7276c3bb4
방상제 <sangje112@naver.com>
2023-09-18 14:14:16 +0900

    chore:<test>test

--- frontend/uniqon/src/stores/WishlistStore.js
+++ frontend/uniqon/src/stores/WishlistStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
+
 let wishlistStore = (set) => ({
   wishlist: [],
   setWishlist: (payload) => set({ wishlist: payload }),
