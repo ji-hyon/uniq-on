ce0c1d7737ba75a90cc1abed15d4219b9e9d2b89
방상제 <sangje112@naver.com>
2023-09-18 15:08:09 +0900

    chore:<wishlist> wishlist store import

--- frontend/uniqon/src/pages/Transaction/Wishlist.jsx
+++ frontend/uniqon/src/pages/Transaction/Wishlist.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { Button } from "@material-tailwind/react";
+import { useWishlistStore } from "../../stores/WishlistStore";
 
 import { useEffect } from "react";
 
