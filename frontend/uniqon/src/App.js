f9385f3322b0e6a2fbcb567ea8235b1fa36b8753
최지수 <cjjss11@naver.com>
2023-09-19 16:54:17 +0900

    feat: 도감 페이지 구성 및 대분류 조회 기능 구현

--- frontend/uniqon/src/App.js
+++ frontend/uniqon/src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button } from "@material-tailwind/react";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import { Collections } from "./pages/Collections/Collections";
-import { Transaction } from "./pages/Transaction/Transaction";
-import { MyPage } from "./pages/Mypage/MyPage";
-import { Wishlist } from "./pages/Transaction/Wishlist";
-import { Landing } from "./pages/Landing/Landing";
-import { NFT } from "./pages/NFT/NFT";
+import { Button } from '@material-tailwind/react';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { Collections } from './pages/Collections/Collections';
+import { Transaction } from './pages/Transaction/Transaction';
+import { MyPage } from './pages/Mypage/MyPage';
+import { Wishlist } from './pages/Transaction/Wishlist';
+import { Landing } from './pages/Landing/Landing';
+import { NFT } from './pages/NFT/NFT';
+import { MainCollections } from './pages/Collections/MainCollections';
+import { MidCollections } from './pages/Collections/MidCollec';
+import { MidCollecDetail } from './pages/Collections/MidCollecDetail';
+import { NFTList } from './pages/Collections/NFTList';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/collections" element={<Collections />} />
+        {/* <Route path="/collections" element={<Collections />} /> */}
         <Route path="/transaction" element={<Transaction />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/nft" element={<NFT />} />
+
+        <Route path="/collections" element={<MainCollections />} />
+        <Route path="/midcollections" element={<MidCollections />} />
+        <Route path="/midcollecdetail" element={<MidCollecDetail />} />
+        <Route path="/nftlist" element={<NFTList />} />
       </Routes>
     </div>
   );
---
+++ frontend/uniqon/src/pages/Collections/NFTList.jsx
@@ -0,0 +1,9 @@
+export function NFTList() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>여기는 포메라니안의 NFT를 보여주는 소분류 페이지 입니다.</p>
+      </header>
+    </div>
+  );
+}
