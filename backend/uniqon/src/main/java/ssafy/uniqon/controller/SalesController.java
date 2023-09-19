930e5f4f8f9eb0b63e28fa973a2dfd8e80375b06
이재명 <iamwoaud1004@naver.com>
2023-09-19 17:30:17 +0900

    fix: 판매글 리스트 조회 기능 수정

--- backend/uniqon/src/main/java/ssafy/uniqon/controller/SalesController.java
+++ backend/uniqon/src/main/java/ssafy/uniqon/controller/SalesController.java
@@ -6,13 +6,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ssafy.uniqon.global.response.Response;
 
-import java.awt.print.Pageable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -78,7 +79,7 @@ public class SalesController {
             @ApiResponse(responseCode = "404", description = "NOT FOUND")
     })
     @GetMapping("/post")
-    public Response<?> getAllPostList(@PageableDefault(size=10)Pageable pageable){
+    public Response<?> getAllPostList(@PageableDefault(size=10) Pageable pageable){
         log.debug("# 판매글 리스트 표시");
         List<postListsWebResponse> postlist = new ArrayList<>();
         postListsWebResponse res = new postListsWebResponse(2, "도마뱀1","lizard","도상제","https://gateway.pinata.cloud/ipfs/QmWorfYFv5TWPpoSvfhqTAGSxq2UZFV8e3L9UnG46BKt6W");
