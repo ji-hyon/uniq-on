b59e5f2c05ca6830cdc8af87b3387361541f1c73
진병욱 <quddnr678@naver.com>
2023-09-20 20:58:52 +0900

    feat: swagger 설정 완료
    Change-Id: I78401cf3ced42e6dd11fa50f40fb1dda6c0d10bd

---
+++ diti/core/src/main/java/com/diti/core/domain/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.diti.core.domain.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/diti/auth")
+@Tag(name = "인증 API")
+@CrossOrigin("*")
+public class AuthController {
+}
---
+++ diti/core/src/main/java/com/diti/core/global/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.diti.core.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("DITI API Document")
+                .description("DITI API 명세서")
+                .version("1.0.0");
+    }
+
+}
