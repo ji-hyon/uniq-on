4a2a6c11262d60a549b630f0bcd270ea4222be55
김한결 <kmr5326@naver.com>
2023-09-21 18:01:56 +0900

    feat: spring security 적용
    Change-Id: I1bfcb502e9ac0ee6858622ddebf9a94ca09c3905

---
+++ backend/uniqon/src/main/java/ssafy/uniqon/global/config/LoginSuccessHandler.java
@@ -0,0 +1,25 @@
+package ssafy.uniqon.global.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        AuthenticationSuccessHandler.super.onAuthenticationSuccess(request, response, chain, authentication);
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+        session.setAttribute("greeting", authentication.getName() + "님 반갑습니다.");
+        response.sendRedirect("/transaction");
+    }
+}
