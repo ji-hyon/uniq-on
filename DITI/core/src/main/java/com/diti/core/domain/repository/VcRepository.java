28000e15b95cd25bd12675b6968f46e0eb2ce6a2
진병욱 <quddnr678@naver.com>
2023-09-22 01:44:05 +0900

    fix: 빠진 파일 추가
    Change-Id: Ie5ec3887a672a5cdd52b568ae1b7ebdd5d70aef0

--- DITI/core/src/main/java/com/diti/core/domain/repository/VcRepository.java
+++ DITI/core/src/main/java/com/diti/core/domain/repository/VcRepository.java
@@ -1,3 +1,12 @@
 package com.diti.core.domain.repository;
-public interface VcRepository {
+
+import com.diti.core.domain.controller.VcController;
+import com.diti.core.domain.entity.Vc;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VcRepository extends JpaRepository<Vc, Integer> {
+
+    Optional<Vc> findByWalletAddressAndType(String walletAddress, String type);
 }
--- DITI/core/src/main/java/com/diti/core/domain/service/VcService.java
+++ DITI/core/src/main/java/com/diti/core/domain/service/VcService.java
@@ -1,6 +1,22 @@
 package com.diti.core.domain.service;
 
+import com.diti.core.domain.controller.VcController;
+
+import java.sql.Timestamp;
+
 public interface VcService {
 
+    public record getVcWebResponse(
+            int id,
+            String walletAddress,
+            String type,
+            String vcJwt,
+            Timestamp createDateTime,
+            Timestamp ModifyDateTime
+
+    ){}
+
     void login(String walletAddress);
+
+    getVcWebResponse getVc(VcController.getVcWebRequest req);
 }
