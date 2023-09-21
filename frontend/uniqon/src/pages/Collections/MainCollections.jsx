270e9ea9b92ddfb82f08ac4b6e84bfaa57ec61a5
최지수 <cjjss11@naver.com>
2023-09-21 09:46:18 +0900

    feat: 대분류, 중분류 구현
    Change-Id: I9aeefe174b3c50412dbb33011a19237f2497c059

--- frontend/uniqon/src/pages/Collections/MainCollections.jsx
+++ frontend/uniqon/src/pages/Collections/MainCollections.jsx
@@ -11,34 +11,49 @@ import {
   IconButton
 } from '@material-tailwind/react';
 import { useCollectionsStore } from '../../stores/CollectionsStore';
+import { useEffect, useState } from 'react';
 
 export function MainCollections() {
   const navigate = useNavigate();
-  const { collections, setCollections } = useCollectionsStore();
-
-  const data = [
-    {
-      id: 1,
-      type: 'fox',
-      image: 'fox.png'
-    },
-    {
-      id: 2,
-      type: 'turtle',
-      image: 'turtle.png'
-    }
-  ];
+  const { mainCollecId, setMainCollecId, mainCollecType, setMainCollecType, mainCollecImg, setMainCollecImg } =
+    useCollectionsStore();
 
   const URL = 'http://localhost:5000';
 
-  async function mainList() {
-    try {
-      const response = await axios.get(URL + '/api/collections/list/main');
-      console.log('success: ', response);
-    } catch (e) {
-      console.log('failed: ', e);
+  const [mainCardsData, setMainCardsData] = useState([]);
+
+  useEffect(() => {
+    async function mainList() {
+      try {
+        const response = await axios.get(URL + `/api/collections/list/main`);
+        console.log('success', response);
+
+        setMainCardsData(response.data.response);
+      } catch (e) {
+        console.log('failed', e);
+      }
     }
-  }
+    mainList();
+  }, []);
+
+  // async function mainList(index) {
+  //   try {
+  //     const response = await axios.get(URL + '/api/collections/list/main');
+  //     console.log('success', response);
+
+  //     setMainCollecId(response.data.response[index].id);
+
+  //     setMainCollecType(response.data.response[index].type);
+
+  //     setMainCollecImg(response.data.response[index].image);
+
+  //     // const selectedCollec = () => {
+  //     //   setCollections(response.data.response[0].id);
+  //     // };
+  //   } catch (e) {
+  //     console.log('failed: ', e);
+  //   }
+  // }
 
   const goToMiddle = () => {
     navigate('/midcollections');
@@ -50,13 +65,11 @@ export function MainCollections() {
         <Button className="text-4xl w-96 h-28" onClick={goToMiddle}>
           강아지
         </Button>
-        <Button className="text-4xl w-96 h-28" onClick={mainList}>
-          대분류 axios 요청
-        </Button>
+
         <div className="flex space-x-4">
-          <Card className="w-full max-w-[26rem] shadow-lg">
+          {/* <Card className="w-full max-w-[26rem] shadow-lg">
             <CardHeader floated={false} color="blue-gray">
-              <img src="fox.png" alt="ui/ux review check" />
+              <img src={mainCollecImg} alt="ui/ux review check" />
               <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
               <IconButton size="sm" color="red" variant="text" className="!absolute top-4 right-4 rounded-full">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
@@ -67,20 +80,20 @@ export function MainCollections() {
             <CardFooter className="pt-5">
               <Button
                 onClick={() => {
-                  setCollections(data[0].id);
+                  mainList(0);
                   goToMiddle();
                 }}
                 className="text-lg"
                 size="lg"
                 fullWidth={true}
               >
-                여우
+                {mainCollecType}
               </Button>
             </CardFooter>
           </Card>
           <Card className="w-full max-w-[26rem] shadow-lg">
             <CardHeader floated={false} color="blue-gray">
-              <img src="turtle.png" alt="ui/ux review check" />
+              <img src={mainCollecImg} alt="ui/ux review check" />
               <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
               <IconButton size="sm" color="red" variant="text" className="!absolute top-4 right-4 rounded-full">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
@@ -91,17 +104,45 @@ export function MainCollections() {
             <CardFooter className="pt-5">
               <Button
                 onClick={() => {
-                  setCollections(data[1].id);
+                  mainList(1);
                   goToMiddle();
                 }}
                 className="text-lg"
                 size="lg"
                 fullWidth={true}
               >
-                거북이
+                {mainCollecType}
               </Button>
             </CardFooter>
-          </Card>
+          </Card> */}
+          {mainCardsData.map((card, index) => (
+            <Card key={index} className="w-full max-w-[26rem] shadow-lg">
+              <CardHeader floated={false} color="blue-gray">
+                <img src={card.image} alt="ui/ux review check" />
+                <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
+                <IconButton size="sm" color="red" variant="text" className="!absolute top-4 right-4 rounded-full">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
+                    <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                  </svg>
+                </IconButton>
+              </CardHeader>
+              <CardFooter className="pt-5">
+                <Button
+                  onClick={() => {
+                    setMainCollecId(card.id);
+                    setMainCollecType(card.type);
+                    setMainCollecImg(card.image);
+                    goToMiddle();
+                  }}
+                  className="text-lg"
+                  size="lg"
+                  fullWidth={true}
+                >
+                  {card.type}
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
         </div>
       </header>
     </div>
--- frontend/uniqon/src/pages/Collections/MidCollec.jsx
+++ frontend/uniqon/src/pages/Collections/MidCollec.jsx
@@ -11,52 +11,148 @@ import {
   Tooltip,
   IconButton
 } from '@material-tailwind/react';
+import { useEffect, useState } from 'react';
 
 export function MidCollections() {
   const navigate = useNavigate();
 
   const URL = 'http://localhost:5000';
 
-  const { collections, setCollections } = useCollectionsStore();
+  const {
+    mainCollecId,
+    setMainCollecId,
+    midCollecId,
+    setMidCollecId,
+    midCollecType,
+    setMidCollecType,
+    midCollecImg,
+    setMidCllecImg
+  } = useCollectionsStore();
 
-  const goToMidCollecDetail = () => {
-    navigate('/midcollecdetail');
+  const [midCardsData, setMidCardsData] = useState([]);
+
+  useEffect(() => {
+    async function middleList() {
+      try {
+        const response = await axios.get(URL + `/api/collections/list/middle/${mainCollecId}`);
+        console.log('success', response);
+
+        setMidCardsData(response.data.response);
+      } catch (e) {
+        console.log('failed: ', e);
+      }
+    }
+    middleList();
+  }, []);
+
+  // const goToMidCollecDetail = () => {
+  //   navigate('/midcollecdetail');
+  // };
+
+  const goToNFTList = () => {
+    navigate('/nftlist');
   };
 
-  async function middleList() {
-    try {
-      const response = await axios.get(URL + `/api/collections/list/middle/${collections}`);
-      console.log('success: ', response);
-    } catch (e) {
-      console.log('failed: ', e);
-    }
-  }
+  // async function middleList(index) {
+  //   try {
+  //     const response = await axios.get(URL + `/api/collections/list/middle/${mainCollecId}`);
+
+  //     setMidCollecId(response.data.response[index].id);
+  //     console.log('data', response.data.response[index].id);
+
+  //     setMidCollecType(response.data.response[index].species);
+  //     console.log('data', response.data.response[index].species);
+
+  //     setMidCllecImg(response.data.response[index].image);
+  //     console.log('data', response.data.response[index].image);
+  //   } catch (e) {
+  //     console.log('failed: ', e);
+  //   }
+  // }
 
   return (
     <div className="App">
       <header className="App-header">
         <p>여기는 강아지에 대한 중분류 페이지 입니다.</p>
-        <Button className="text-4xl w-96 h-28" onClick={goToMidCollecDetail}>
-          포메라니안
-        </Button>
-        <Button onClick={middleList}>중분류 axios 요청</Button>
 
-        <Card className="w-full max-w-[26rem] shadow-lg">
-          <CardHeader floated={false} color="blue-gray">
-            <img src="fox.png" alt="ui/ux review check" />
-            <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-            <IconButton size="sm" color="red" variant="text" className="!absolute top-4 right-4 rounded-full">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
-                <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-              </svg>
-            </IconButton>
-          </CardHeader>
-          <CardFooter className="pt-5">
-            <Button className="text-lg" size="lg" fullWidth={true}>
-              붉은 여우
-            </Button>
-          </CardFooter>
-        </Card>
+        <div className="flex space-x-4">
+          {midCardsData.map((card, index) => (
+            <Card key={index} className="w-full max-w-[26rem] shadow-lg">
+              <CardHeader floated={false} color="blue-gray">
+                <img src={card.image} alt="ui/ux review check" />
+                <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
+                <IconButton size="sm" color="red" variant="text" className="!absolute top-4 right-4 rounded-full">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
+                    <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                  </svg>
+                </IconButton>
+              </CardHeader>
+              <CardFooter className="pt-5">
+                <Button
+                  onClick={() => {
+                    setMidCollecId(card.id);
+                    setMidCollecType(card.type);
+                    setMidCllecImg(card.image);
+                    goToNFTList();
+                  }}
+                  className="text-lg"
+                  size="lg"
+                  fullWidth={true}
+                >
+                  {card.species}
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+          {/* <Card className="w-full max-w-[26rem] shadow-lg">
+            <CardHeader floated={false} color="blue-gray">
+              <img src={midCollecImg} alt="ui/ux review check" />
+              <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
+              <IconButton size="sm" color="red" variant="text" className="!absolute top-4 right-4 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
+                  <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                </svg>
+              </IconButton>
+            </CardHeader>
+            <CardFooter className="pt-5">
+              <Button
+                onClick={() => {
+                  middleList(0);
+                  goToMidCollecDetail();
+                }}
+                className="text-lg"
+                size="lg"
+                fullWidth={true}
+              >
+                {midCollecType}
+              </Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-full max-w-[26rem] shadow-lg">
+            <CardHeader floated={false} color="blue-gray">
+              <img src={midCollecImg} alt="ui/ux review check" />
+              <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
+              <IconButton size="sm" color="red" variant="text" className="!absolute top-4 right-4 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
+                  <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                </svg>
+              </IconButton>
+            </CardHeader>
+            <CardFooter className="pt-5">
+              <Button
+                onClick={() => {
+                  middleList(1);
+                  goToMidCollecDetail();
+                }}
+                className="text-lg"
+                size="lg"
+                fullWidth={true}
+              >
+                {midCollecType}
+              </Button>
+            </CardFooter>
+          </Card> */}
+        </div>
       </header>
     </div>
   );
