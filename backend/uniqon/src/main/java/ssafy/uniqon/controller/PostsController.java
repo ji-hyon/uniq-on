58fd65cfc90bde3a1635c92a988799a0ef9344f8
이재명 <iamwoaud1004@naver.com>
2023-09-22 22:57:15 +0900

    feature: 판매글 상세페이지 Get 기능 구현
    Change-Id: I1645a9bbfec0081eca9906459ae072443ea53255

--- backend/uniqon/src/main/java/ssafy/uniqon/controller/PostsController.java
+++ backend/uniqon/src/main/java/ssafy/uniqon/controller/PostsController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import ssafy.uniqon.global.response.Response;
 import ssafy.uniqon.service.PostCreateService;
 import ssafy.uniqon.service.PostReadService;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.List;
@@ -43,6 +45,21 @@ public class PostsController {
             String image
     )
     {}
+
+    public record postDetailWebResponse(
+            String profileImage,
+            String nickname,
+            String species,
+            String name,
+            String feature,
+            Integer age,
+            String image,
+            Integer price,
+            String content,
+            Timestamp createDatetime,
+            String title,
+            Boolean wishCheck
+    ){}
 private final PostReadService postReadService;
 private final PostCreateService postCreateService;
     @Operation(summary="판매글 등록", description = "판매글을 등록합니다.")
@@ -73,9 +90,43 @@ private final PostCreateService postCreateService;
         return OK(null);
     }
 
+    @Operation(summary="판매글 상세 조회", description = "판매글 상세 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND")
+    })
     @GetMapping("/detail/{postId}")
-    public Response<?> getDetailPost(@PathVariable int postId){
-        return OK(null);
+    public Response<?> getDetailPost(@PathVariable int postId, @RequestParam String walletAddress){
+        log.debug("# 판매글 식별자 id : {}", postId);
+        log.debug("# 사용자 walletAddress : {}", walletAddress);
+
+        postDetailWebResponse post = postReadService.getPostDetail(postId, walletAddress);
+
+        JSONObject obj = new JSONObject();
+        JSONObject data1=new JSONObject();
+        JSONObject data2=new JSONObject();
+        JSONObject data3=new JSONObject();
+
+        data1.put("profileImage",post.profileImage);
+        data1.put("nickname",post.nickname);
+        obj.put("SellerInfo",data1);
+
+        data2.put("species",post.species);
+        data2.put("name",post.name);
+        data2.put("feature",post.feature);
+        data2.put("age",post.age);
+        data2.put("image",post.image);
+        obj.put("nftInfo",data2);
+
+        data3.put("price",post.price);
+        data3.put("content",post.content);
+        data3.put("createDatetime",post.createDatetime);
+        data3.put("title",post.title);
+        data3.put("wishCheck",post.wishCheck);
+        obj.put("PostInfo",data3);
+
+        return OK(obj);
     }
 
     @Operation(summary="판매글 조회", description = "판매글 리스트를 조회합니다.")
