58de464374d115c42281571556dac54310558bb4
ji-hyon <gyg03134@gmail.com>
2023-09-23 20:35:56 +0900

    feat: DITI에 VC 등록 Node 요청 Spring 연결
    Change-Id: Ia8d608adb0ad3ac4845e1eec0b848f510cf48ebb

--- DITI/nodeJs/routes/didRouter.js
+++ DITI/nodeJs/routes/didRouter.js
@@ -9,6 +9,7 @@ import { verifyLoginMessage } from "../src/auth.js"
 
 import { readImage } from "../src/ocr.js"
 import { createVC } from "../src/did.js"
+import axios from "axios"
 
 // /vc 경로로 post 요청이 왔을 때의 로직
 // https://inpa.tistory.com/entry/EXPRESS-%F0%9F%93%9A-multer-%EB%AF%B8%EB%93%A4%EC%9B%A8%EC%96%B4
@@ -19,10 +20,39 @@ router.post('/vc', upload.single("imgFile"), async (req, res) => {
         res.status(400).send("login failed")
     }
 
-    // OCR로 읽어들인 정보 처리 아래와 같이 진행하면 될듯  
+    // OCR로 읽어들인 정보 처리 
     // console.log("log:",req.file)
     const ocrResult=await readImage(req.file)
     const vcJwt = await createVC(walletAddress, ocrResult)
+    // 유저 테이블에 주소가 추가되는 것 (VC없이 지갑주소만 추가)
+    try {
+        const springJoin = await axios.post(process.env.SPRING_SERVER_URI + "/diti/auth/join",
+            walletAddress,
+            {
+                headers: {
+                    'Content-Type': 'text/plain',
+                }
+            })
+    } catch(e) {
+        console.log(e)
+        console.log("/diti/auth/join failed")
+    }
+    // VC 테이블에 vcJwt 추가 
+    try {
+        const springResponse = await axios.post(process.env.SPRING_SERVER_URI + "/diti/vc",
+            {
+                walletAddress: walletAddress,
+                vcJwt: vcJwt,
+                type: "idCard",
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+    } catch {
+        console.log("/diti/vc failed")
+    }
     res.send(vcJwt)
 })
 
