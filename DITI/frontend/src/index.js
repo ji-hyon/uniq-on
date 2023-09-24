2555f3728a49d0c0e52e08ee3012306629c92497
ji-hyon <gyg03134@gmail.com>
2023-09-25 02:34:33 +0900

    test: reverse proxy 테스트2
    Change-Id: I3d498b050b24fc0b812757b48643f9dadb8682b2

--- DITI/frontend/src/index.js
+++ DITI/frontend/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/diti">
         <App />
       </BrowserRouter>
     </ThemeProvider>
