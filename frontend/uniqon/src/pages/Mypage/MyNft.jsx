10d32a8c5de76bd08b59c99a27859dd1634b1d37
진병욱 <quddnr678@naver.com>
2023-09-27 00:46:29 +0900

    fix: nft 리스트 조회 css 변경
    Change-Id: I6e92d17868f3da08c1df5a2a72c31c2cbbc81a29

--- frontend/uniqon/src/pages/Mypage/MyNft.jsx
+++ frontend/uniqon/src/pages/Mypage/MyNft.jsx
@@ -53,12 +53,12 @@ export function MyNft() {
   return (
     <div className="App">
       <p>나의 NFT</p>
-      <div className="flex w-[1200px] items-start gap-[32px] relative flex-wrap">
-        {nftList.map((nft, index) => (
-          <div
-            key={index}
-            className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]"
-          >
+      {nftList.map((nft, index) => (
+        <div
+          key={index}
+          className="flex w-[1200px] items-start gap-[32px] relative flex-wrap"
+        >
+          <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
             {/* 카드 넣기 */}
             <Card className="w-full max-w-[20rem] shadow-lg">
               <CardHeader floated={false} color="blue-gray">
@@ -72,10 +72,10 @@ export function MyNft() {
               </CardFooter>
             </Card>
           </div>
-        ))}
-        <Button onClick={handlePreviousPage}>이전 페이지</Button>
-        <Button onClick={handleNextPage}>다음 페이지</Button>
-      </div>
+        </div>
+      ))}
+      <Button onClick={handlePreviousPage}>이전 페이지</Button>
+      <Button onClick={handleNextPage}>다음 페이지</Button>
     </div>
   );
 }
