65ee3cdf3b5d157430c7182f67d68c7e5e3a08a4
진병욱 <quddnr678@naver.com>
2023-09-27 00:57:13 +0900

    feat: mypage 구매내역
    Change-Id: Ibea62352399c20f02cbe88513d8e0fecc3fcaba7

--- frontend/uniqon/src/pages/Mypage/Sales.jsx
+++ frontend/uniqon/src/pages/Mypage/Sales.jsx
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import {
   Button,
   Card,
@@ -7,73 +8,43 @@ import {
   CardFooter,
   Typography,
   Tooltip,
-  IconButton
-} from '@material-tailwind/react';
+  IconButton,
+} from "@material-tailwind/react";
 
 export function Sales() {
   // 판매 내역
-  async function salesList() {
-    try {
-      const response = await axios.get(`/api/myPage/soldList/${1}`);
-      console.log('성공', response);
-    } catch (error) {
-      console.log('실패', error);
+  const [salesList, setSalesList] = useState([]);
+  const [page, setPage] = useState(0);
+  const [totalPage, setTotalPage] = useState(0);
+  useEffect(() => {
+    async function salesList() {
+      try {
+        const response = await axios.get(`/api/myPage/soldList`, {
+          params: {
+            page: page,
+            size: 10,
+          },
+        });
+        if (response.success) {
+          console.log("성공", response);
+          setSalesList(response.resonse.content);
+          setTotalPage(response.resonse.tototalPages);
+        }
+      } catch (error) {
+        console.log("실패", error);
+      }
     }
-  }
+  }, [page]);
 
   return (
     <div className="App">
       <p>판매이력</p>
-      <div className="flex w-[1200px] items-start gap-[32px] relative flex-wrap">
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          {/* 카드 넣기 */}
-          <Card className="w-full max-w-[20rem] shadow-lg">
-            <CardHeader floated={false} color="blue-gray">
-              <img src="fox.png" alt="ui/ux review check" />
-              <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-            </CardHeader>
-            <CardFooter className="pt-3 pb-3">
-              <Button className="text-sm" size="sm" fullWidth={true}>
-                여우
-              </Button>
-            </CardFooter>
-          </Card>
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          {/* <div className="inline-flex flex-col items-start relative flex-[0_0_auto]"> */}
-          {/* 카드 넣기 */}
-          <Card className="w-full max-w-[20rem] shadow-lg">
-            <CardHeader floated={false} color="blue-gray">
-              <img src="fox.png" alt="ui/ux review check" />
-              <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-            </CardHeader>
-            <CardFooter className="pt-3 pb-3">
-              <Button className="text-sm" size="sm" fullWidth={true}>
-                여우
-              </Button>
-            </CardFooter>
-          </Card>
-          {/* </div> */}
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] mr-[-96.00px] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          {/* <div className="inline-flex flex-col items-start relative flex-[0_0_auto]"> */}
-          {/* 카드넣기 */}
-          <Card className="w-full max-w-[20rem] shadow-lg">
-            <CardHeader floated={false} color="blue-gray">
-              <img src="fox.png" alt="ui/ux review check" />
-              <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-            </CardHeader>
-            <CardFooter className="pt-3 pb-3">
-              <Button className="text-sm" size="sm" fullWidth={true}>
-                여우
-              </Button>
-            </CardFooter>
-          </Card>
-          {/* <div className="top-[236px] absolute w-[340px] h-px left-0 bg-[#0000000d]" /> */}
-          {/* </div> */}
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+      {salesList.map((sales, index) => (
+        <div
+          key={index}
+          className="flex w-[1200px] items-start gap-[32px] relative flex-wrap"
+        >
+          <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
             {/* 카드 넣기 */}
             <Card className="w-full max-w-[20rem] shadow-lg">
               <CardHeader floated={false} color="blue-gray">
@@ -82,45 +53,14 @@ export function Sales() {
               </CardHeader>
               <CardFooter className="pt-3 pb-3">
                 <Button className="text-sm" size="sm" fullWidth={true}>
-                  여우
+                  {sales.title}
+                  {/* 이부분 한결이형이랑 체크 */}
                 </Button>
               </CardFooter>
             </Card>
           </div>
         </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
-            {/* 카드 넣기 */}
-            <Card className="w-full max-w-[20rem] shadow-lg">
-              <CardHeader floated={false} color="blue-gray">
-                <img src="fox.png" alt="ui/ux review check" />
-                <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-              </CardHeader>
-              <CardFooter className="pt-3 pb-3">
-                <Button className="text-sm" size="sm" fullWidth={true}>
-                  여우
-                </Button>
-              </CardFooter>
-            </Card>
-          </div>
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
-            {/* 카드 넣기 */}
-            <Card className="w-full max-w-[20rem] shadow-lg">
-              <CardHeader floated={false} color="blue-gray">
-                <img src="fox.png" alt="ui/ux review check" />
-                <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-              </CardHeader>
-              <CardFooter className="pt-3 pb-3">
-                <Button className="text-sm" size="sm" fullWidth={true}>
-                  여우
-                </Button>
-              </CardFooter>
-            </Card>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
