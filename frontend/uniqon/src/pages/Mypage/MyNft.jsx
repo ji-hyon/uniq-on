3caaacff3fc2c3b6f15e533cd1286f68c01f7a2e
진병욱 <quddnr678@naver.com>
2023-09-27 00:44:23 +0900

    feat: 마이페이지 nft 리스트 조회
    Change-Id: Ia1b4a20576b103c40c756daf1284b5378c38f0aa

--- frontend/uniqon/src/pages/Mypage/MyNft.jsx
+++ frontend/uniqon/src/pages/Mypage/MyNft.jsx
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import {
   Button,
   Card,
@@ -7,73 +8,57 @@ import {
   CardFooter,
   Typography,
   Tooltip,
-  IconButton
-} from '@material-tailwind/react';
+  IconButton,
+} from "@material-tailwind/react";
 
 export function MyNft() {
+  const [nftList, setNftList] = useState([]);
+  const [page, setPage] = useState(0);
+  const [totalPage, setTotalPage] = useState(0);
   // 나의 NFT 조회
-  async function getMyNft() {
-    try {
-      const resonse = await axios.get(`/api/mypage/mynfts/${1}`);
-      console.log('성공', resonse);
-    } catch (error) {
-      console.log('실패', error);
+  useEffect(() => {
+    async function getMyNft() {
+      try {
+        const resonse = await axios.get(`/api/mypage/mynfts`, {
+          params: {
+            page: page,
+            size: 10,
+          },
+        });
+        console.log(resonse);
+        if (resonse.success) {
+          console.log("성공", resonse);
+          setNftList(resonse.resonse.content);
+          setTotalPage(resonse.resonse.tototalPages);
+        }
+      } catch (error) {
+        console.log("실패", error);
+      }
     }
-  }
+    getMyNft();
+  }, [page]);
+
+  const handlePreviousPage = () => {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (page <= totalPage) {
+      setPage(page + 1);
+    }
+  };
 
   return (
     <div className="App">
       <p>나의 NFT</p>
       <div className="flex w-[1200px] items-start gap-[32px] relative flex-wrap">
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          {/* 카드 넣기 */}
-          <Card className="w-full max-w-[20rem] shadow-lg">
-            <CardHeader floated={false} color="blue-gray">
-              <img src="fox.png" alt="ui/ux review check" />
-              <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-            </CardHeader>
-            <CardFooter className="pt-3 pb-3">
-              <Button className="text-sm" size="sm" fullWidth={true}>
-                여우
-              </Button>
-            </CardFooter>
-          </Card>
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          {/* <div className="inline-flex flex-col items-start relative flex-[0_0_auto]"> */}
-          {/* 카드 넣기 */}
-          <Card className="w-full max-w-[20rem] shadow-lg">
-            <CardHeader floated={false} color="blue-gray">
-              <img src="fox.png" alt="ui/ux review check" />
-              <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-            </CardHeader>
-            <CardFooter className="pt-3 pb-3">
-              <Button className="text-sm" size="sm" fullWidth={true}>
-                여우
-              </Button>
-            </CardFooter>
-          </Card>
-          {/* </div> */}
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] mr-[-96.00px] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          {/* <div className="inline-flex flex-col items-start relative flex-[0_0_auto]"> */}
-          {/* 카드넣기 */}
-          <Card className="w-full max-w-[20rem] shadow-lg">
-            <CardHeader floated={false} color="blue-gray">
-              <img src="fox.png" alt="ui/ux review check" />
-              <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-            </CardHeader>
-            <CardFooter className="pt-3 pb-3">
-              <Button className="text-sm" size="sm" fullWidth={true}>
-                여우
-              </Button>
-            </CardFooter>
-          </Card>
-          {/* <div className="top-[236px] absolute w-[340px] h-px left-0 bg-[#0000000d]" /> */}
-          {/* </div> */}
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+        {nftList.map((nft, index) => (
+          <div
+            key={index}
+            className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]"
+          >
             {/* 카드 넣기 */}
             <Card className="w-full max-w-[20rem] shadow-lg">
               <CardHeader floated={false} color="blue-gray">
@@ -82,44 +67,14 @@ export function MyNft() {
               </CardHeader>
               <CardFooter className="pt-3 pb-3">
                 <Button className="text-sm" size="sm" fullWidth={true}>
-                  여우
+                  {nft.name}
                 </Button>
               </CardFooter>
             </Card>
           </div>
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
-            {/* 카드 넣기 */}
-            <Card className="w-full max-w-[20rem] shadow-lg">
-              <CardHeader floated={false} color="blue-gray">
-                <img src="fox.png" alt="ui/ux review check" />
-                <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-              </CardHeader>
-              <CardFooter className="pt-3 pb-3">
-                <Button className="text-sm" size="sm" fullWidth={true}>
-                  여우
-                </Button>
-              </CardFooter>
-            </Card>
-          </div>
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
-            {/* 카드 넣기 */}
-            <Card className="w-full max-w-[20rem] shadow-lg">
-              <CardHeader floated={false} color="blue-gray">
-                <img src="fox.png" alt="ui/ux review check" />
-                <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-              </CardHeader>
-              <CardFooter className="pt-3 pb-3">
-                <Button className="text-sm" size="sm" fullWidth={true}>
-                  여우
-                </Button>
-              </CardFooter>
-            </Card>
-          </div>
-        </div>
+        ))}
+        <Button onClick={handlePreviousPage}>이전 페이지</Button>
+        <Button onClick={handleNextPage}>다음 페이지</Button>
       </div>
     </div>
   );
