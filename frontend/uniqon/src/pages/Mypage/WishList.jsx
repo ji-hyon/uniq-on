f5716c8ba4147ecebaa880e4cab75d67ce2a3168
진병욱 <quddnr678@naver.com>
2023-09-27 11:39:38 +0900

    fix: mypage nftList div 감싸는 것 변경
    Change-Id: If457ea2bd247c62276122f35d458cd6d642b75e2

--- frontend/uniqon/src/pages/Mypage/WishList.jsx
+++ frontend/uniqon/src/pages/Mypage/WishList.jsx
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
+import "./card.css";
 import {
   Button,
   Card,
@@ -7,17 +8,23 @@ import {
   CardFooter,
   Typography,
   Tooltip,
-  IconButton
-} from '@material-tailwind/react';
+  IconButton,
+} from "@material-tailwind/react";
 
 export function WishList() {
   // 위시리스트 조회
+  const wishLIst = [
+    { name: "제목1" },
+    { name: "제목2" },
+    { name: "제목3" },
+    { name: "제목4" },
+  ];
   async function likeCollecList() {
     try {
       const resonse = await axios.get(`/api/mypage/like-nft-list/${1}`);
-      console.log('성공', resonse);
+      console.log("성공", resonse);
     } catch (error) {
-      console.log('실패', error);
+      console.log("실패", error);
     }
   }
 
@@ -25,101 +32,28 @@ export function WishList() {
     <div className="App">
       <p>위시리스트</p>
       <div className="flex w-[1200px] items-start gap-[32px] relative flex-wrap">
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          {/* 카드 넣기 */}
-          <Card className="w-full max-w-[20rem] shadow-lg">
-            <CardHeader floated={false} color="blue-gray">
-              <img src="fox.png" alt="ui/ux review check" />
-              <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-            </CardHeader>
-            <CardFooter className="pt-3 pb-3">
-              <Button className="text-sm" size="sm" fullWidth={true}>
-                여우
-              </Button>
-            </CardFooter>
-          </Card>
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          {/* <div className="inline-flex flex-col items-start relative flex-[0_0_auto]"> */}
-          {/* 카드 넣기 */}
-          <Card className="w-full max-w-[20rem] shadow-lg">
-            <CardHeader floated={false} color="blue-gray">
-              <img src="fox.png" alt="ui/ux review check" />
-              <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-            </CardHeader>
-            <CardFooter className="pt-3 pb-3">
-              <Button className="text-sm" size="sm" fullWidth={true}>
-                여우
-              </Button>
-            </CardFooter>
-          </Card>
-          {/* </div> */}
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] mr-[-96.00px] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          {/* <div className="inline-flex flex-col items-start relative flex-[0_0_auto]"> */}
-          {/* 카드넣기 */}
-          <Card className="w-full max-w-[20rem] shadow-lg">
-            <CardHeader floated={false} color="blue-gray">
-              <img src="fox.png" alt="ui/ux review check" />
-              <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-            </CardHeader>
-            <CardFooter className="pt-3 pb-3">
-              <Button className="text-sm" size="sm" fullWidth={true}>
-                여우
-              </Button>
-            </CardFooter>
-          </Card>
-          {/* <div className="top-[236px] absolute w-[340px] h-px left-0 bg-[#0000000d]" /> */}
-          {/* </div> */}
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+        {wishLIst.map((index, wishlist) => (
+          <div
+            key={index}
+            className="flip inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]"
+          >
             {/* 카드 넣기 */}
-            <Card className="w-full max-w-[20rem] shadow-lg">
-              <CardHeader floated={false} color="blue-gray">
-                <img src="fox.png" alt="ui/ux review check" />
-                <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-              </CardHeader>
-              <CardFooter className="pt-3 pb-3">
-                <Button className="text-sm" size="sm" fullWidth={true}>
-                  여우
-                </Button>
-              </CardFooter>
+            <Card className="card w-full max-w-[20rem] shadow-lg">
+              <div className="front">
+                <CardHeader floated={false} color="blue-gray">
+                  <img src="fox.png" alt="ui/ux review check" />
+                  <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
+                </CardHeader>
+                <CardFooter className="pt-3 pb-3">
+                  <Button className="text-sm" size="sm" fullWidth={true}>
+                    {wishLIst.name}
+                  </Button>
+                </CardFooter>
+              </div>
+              <div className="back">{wishLIst.name}</div>
             </Card>
           </div>
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
-            {/* 카드 넣기 */}
-            <Card className="w-full max-w-[20rem] shadow-lg">
-              <CardHeader floated={false} color="blue-gray">
-                <img src="fox.png" alt="ui/ux review check" />
-                <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-              </CardHeader>
-              <CardFooter className="pt-3 pb-3">
-                <Button className="text-sm" size="sm" fullWidth={true}>
-                  여우
-                </Button>
-              </CardFooter>
-            </Card>
-          </div>
-        </div>
-        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-          <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
-            {/* 카드 넣기 */}
-            <Card className="w-full max-w-[20rem] shadow-lg">
-              <CardHeader floated={false} color="blue-gray">
-                <img src="fox.png" alt="ui/ux review check" />
-                <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-              </CardHeader>
-              <CardFooter className="pt-3 pb-3">
-                <Button className="text-sm" size="sm" fullWidth={true}>
-                  여우
-                </Button>
-              </CardFooter>
-            </Card>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
