5cc9b47af606dab6655b5315e8737570d92e183a
진병욱 <quddnr678@naver.com>
2023-09-27 11:39:38 +0900

    fix: mypage nftList div 감싸는 것 변경
    Change-Id: If457ea2bd247c62276122f35d458cd6d642b75e2

--- frontend/uniqon/src/pages/Mypage/MyNft.jsx
+++ frontend/uniqon/src/pages/Mypage/MyNft.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+
 import {
   Button,
   Card,
@@ -53,27 +54,26 @@ export function MyNft() {
   return (
     <div className="App">
       <p>나의 NFT</p>
-      {nftList.map((nft, index) => (
-        <div
-          key={index}
-          className="flex w-[1200px] items-start gap-[32px] relative flex-wrap"
-        >
-          <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
-            {/* 카드 넣기 */}
-            <Card className="w-full max-w-[20rem] shadow-lg">
-              <CardHeader floated={false} color="blue-gray">
-                <img src="fox.png" alt="ui/ux review check" />
-                <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-              </CardHeader>
-              <CardFooter className="pt-3 pb-3">
-                <Button className="text-sm" size="sm" fullWidth={true}>
-                  {nft.name}
-                </Button>
-              </CardFooter>
-            </Card>
-          </div>
+      <div className="flex w-[1200px] items-start gap-[32px] relative flex-wrap">
+        <div className="inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]">
+          {nftList.map((nft, index) => (
+            <div>
+              {/* 카드 넣기 */}
+              <Card key={index} className="w-full max-w-[20rem] shadow-lg">
+                <CardHeader floated={false} color="blue-gray">
+                  <img src="fox.png" alt="ui/ux review check" />
+                  <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
+                </CardHeader>
+                <CardFooter className="pt-3 pb-3">
+                  <Button className="text-sm" size="sm" fullWidth={true}>
+                    {nft}
+                  </Button>
+                </CardFooter>
+              </Card>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
       <Button onClick={handlePreviousPage}>이전 페이지</Button>
       <Button onClick={handleNextPage}>다음 페이지</Button>
     </div>
