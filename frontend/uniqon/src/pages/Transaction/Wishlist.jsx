8ccdba3380eee1335ec86fa25adb1fe4825ae713
진병욱 <quddnr678@naver.com>
2023-09-29 18:51:22 +0900

    fix: axios 요청에 대한 response 및 에러 처리 수정
    Change-Id: I5062e54a91e32426b214543601c9f9681bcefc3a

--- frontend/uniqon/src/pages/Transaction/Wishlist.jsx
+++ frontend/uniqon/src/pages/Transaction/Wishlist.jsx
@@ -35,7 +35,14 @@ export function Wishlist() {
           size: 10, // 추후에 불러올 만큼 사이즈 설정
         },
       });
-      console.log(res.data.response);
+      if (res.status === 200 && res.data.success) {
+        console.log(res.data.response.content);
+      } else if (res.data.error.status === 404) {
+        alert("위시리스트가 존재하지 않습니다");
+        console.log(res);
+      } else {
+        console.log(res);
+      }
     } catch (err) {
       console.log(err);
     }
