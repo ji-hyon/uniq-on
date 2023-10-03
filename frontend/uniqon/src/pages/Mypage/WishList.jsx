41350fe995f9fd407e743330756ed81fc406c903
진병욱 <quddnr678@naver.com>
2023-10-03 15:31:03 +0900

    feat: 내가 좋아요한 도감 작업중
    Change-Id: Ida00ff59e1c7db691dae3e7edac8a7aab1ce6e5f

--- frontend/uniqon/src/pages/Mypage/WishList.jsx
+++ frontend/uniqon/src/pages/Mypage/WishList.jsx
@@ -10,6 +10,7 @@ import {
   Tooltip,
   IconButton,
 } from "@material-tailwind/react";
+import { useEffect } from "react";
 
 export function WishList() {
   // 위시리스트 조회
@@ -19,20 +20,23 @@ export function WishList() {
     { name: "제목3" },
     { name: "제목4" },
   ];
-  async function likeCollecList() {
-    try {
-      const resonse = await axios.get(`/api/mypage/like-nft-list/${1}`);
-      console.log("성공", resonse);
-    } catch (error) {
-      console.log("실패", error);
+  useEffect(() => {
+    async function likeCollecList() {
+      try {
+        const resonse = await axios.get(`/api/mypage/like-nft-list`);
+        console.log("성공", resonse);
+      } catch (error) {
+        console.log("실패", error);
+      }
     }
-  }
+    likeCollecList();
+  }, []);
 
   return (
     <div className="App">
       <p>내가 좋아요 한 도감</p>
       <div className="flex w-[1200px] items-start gap-[32px] relative flex-wrap">
-        {wishLIst.map((index, wishlist) => (
+        {wishLIst.map((index, wishLIst) => (
           <div
             key={index}
             className="flip inline-flex flex-col min-w-[320px] items-start relative flex-[0_0_auto] bg-white rounded-[8px] overflow-hidden shadow-[0px_8px_40px_#0000000a,0px_2px_5px_#0000000d,0px_0px_2px_#00000026]"
