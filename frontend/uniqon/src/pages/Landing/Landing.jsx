6eb623ea226987a57542015069c8aa4974dfa645
김한결 <kmr5326@naver.com>
2023-10-04 15:22:59 +0900

    style: 안 쓰는 버튼 제거
    Change-Id: I09d4484ac716b1f548a2f379f5ea411aa80ba60b

--- frontend/uniqon/src/pages/Landing/Landing.jsx
+++ frontend/uniqon/src/pages/Landing/Landing.jsx
@@ -44,64 +44,7 @@ export function Landing() {
         >
           회원가입
         </Button>
-
-        <Button
-          className="text-4xl w-96 h-28"
-          onClick={() => {
-            navigate("/collections");
-
-          }}
-          color="red"
-        >
-
-          도감
-        </Button>
-        <Button
-          className="text-4xl w-96 h-28"
-          onClick={() => {
-            navigate("/mypage");
-
-          }}
-          color="yellow"
-        >
-
-          마이페이지
-        </Button>
-        <Button
-          className="text-4xl w-96 h-28"
-          onClick={() => {
-            navigate("/transaction");
-
-          }}
-          color="black"
-        >
-
-          거래
-        </Button>
-        <Button
-          className="text-4xl w-96 h-28"
-          onClick={() => {
-            navigate("/wishlist");
-
-          }}
-          color="blue"
-        >
-
-          위시리스트
-        </Button>
-        <Button
-          className="text-4xl w-96 h-28"
-          onClick={() => {
-            navigate("/nft");
-
-          }}
-          color="purple"
-        >
-
-          NFT
-        </Button>
-
-        
+       
 
       </header>
 
