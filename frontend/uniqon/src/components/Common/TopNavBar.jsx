8600e29f3421821066bb0d1be94e68fb07352b01
최지수 <cjjss11@naver.com>
2023-10-04 14:53:46 +0900

    feat: 알림 없을 시 없음 표시 기능
    Change-Id: I7ebae3153771cc1207d3641bfccfeb3e140f8ebc

--- frontend/uniqon/src/components/Common/TopNavBar.jsx
+++ frontend/uniqon/src/components/Common/TopNavBar.jsx
@@ -154,24 +154,27 @@ export function TopNavBar() {
               }}
             >
               <ul style={{ color: "black" }}>
-                {notifications.map((notification, index) => (
-                  <li key={index} style={{ fontSize: "15px" }}>
-                    <div
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        marginLeft: "5px"
-                      }}
-                    >
-                      <TiMediaRecord
-                        style={{ marginRight: "0.5rem", fontSize: "15px" }}
-                      ></TiMediaRecord>
-                      등록하신 "{notification.postTitle}" 판매 글의 NFT가 판매
-                      되었습니다.
-                      {/* {notification.content} */}
-                    </div>
+                {notifications.length === 0 ? (
+                  <li style={{ fontSize: "15px" }}>알림이 없어용~</li>
+                ) : (
+                  notifications.map((notification, index) => (
+                    <li key={index} style={{ fontSize: "15px" }}>
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          marginLeft: "5px"
+                        }}
+                      >
+                        <TiMediaRecord
+                          style={{ marginRight: "0.5rem", fontSize: "15px" }}
+                        ></TiMediaRecord>
+                        등록하신 "{notification.postTitle}" 판매 글의 NFT가 판매
+                        되었습니다.
+                        {/* {notification.content} */}
+                      </div>
 
-                    {/* <Button
+                      {/* <Button
                       onClick={deleteNotification(notification)}
                       variant="outlined"
                       color="red"
@@ -179,11 +182,12 @@ export function TopNavBar() {
                     >
                       삭제
                     </Button> */}
-                    {index !== notifications.length - 1 && (
-                      <hr className="border-t border-gray-300 my-2"></hr>
-                    )}
-                  </li>
-                ))}
+                      {index !== notifications.length - 1 && (
+                        <hr className="border-t border-gray-300 my-2"></hr>
+                      )}
+                    </li>
+                  ))
+                )}
               </ul>
             </div>
           )}
