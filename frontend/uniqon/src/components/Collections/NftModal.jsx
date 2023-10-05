6302825a4b20fe58754c708ec9e8c81c1c328242
최지수 <cjjss11@naver.com>
2023-10-06 04:12:41 +0900

    style: 모달창 내용 스타일 변경
    Change-Id: I358a46405f9eacef6d5d80ff0eadf941dce1ee2e

--- frontend/uniqon/src/components/Collections/NftModal.jsx
+++ frontend/uniqon/src/components/Collections/NftModal.jsx
@@ -40,8 +40,7 @@ export function NftModal({ isOpen, onClose, selectedNft }) {
               color: "black",
               display: "flex",
               flexDirection: "column",
-              fontSize: "20px",
-              marginRight: "200px"
+              fontSize: "20px"
             }}
           >
             <div
@@ -52,8 +51,7 @@ export function NftModal({ isOpen, onClose, selectedNft }) {
                 alignItems: "center"
               }}
             >
-              <TiMediaRecord style={{ marginRight: "0.5rem" }}></TiMediaRecord>
-              <p>소유주: {selectedNft.nickname}</p>
+              <p>- 소유주: {selectedNft.nickname}</p>
             </div>
             <div
               style={{
@@ -63,8 +61,7 @@ export function NftModal({ isOpen, onClose, selectedNft }) {
                 alignItems: "center"
               }}
             >
-              <TiMediaRecord style={{ marginRight: "0.5rem" }}></TiMediaRecord>
-              <p>동물 이름: {selectedNft.name}</p>
+              <p>- 동물 이름: {selectedNft.name}</p>
             </div>
             <div
               style={{
@@ -74,8 +71,7 @@ export function NftModal({ isOpen, onClose, selectedNft }) {
                 alignItems: "center"
               }}
             >
-              <TiMediaRecord style={{ marginRight: "0.5rem" }}></TiMediaRecord>
-              <p>나이: {selectedNft.age}</p>
+              <p>- 나이: {selectedNft.age}</p>
             </div>
             <div
               style={{
@@ -85,8 +81,7 @@ export function NftModal({ isOpen, onClose, selectedNft }) {
                 alignItems: "center"
               }}
             >
-              <TiMediaRecord style={{ marginRight: "0.5rem" }}></TiMediaRecord>
-              <p>특징: {selectedNft.feature}</p>
+              <p>- 특징: {selectedNft.feature}</p>
             </div>
           </div>
           <hr className="border-t border-gray-300 my-4"></hr>
@@ -98,83 +93,6 @@ export function NftModal({ isOpen, onClose, selectedNft }) {
         </CardFooter>
       </Card>
     </Dialog>
-
-    // <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-    //   {/* <div className="modal-bg fixed top-0 left-0 w-full h-full bg-black opacity-50" /> */}
-    //   <div
-    //     className="modal-content bg-white p-4 rounded-lg shadow-md"
-    //     style={{ maxWidth: "30rem" }}
-    //   >
-    //     {/* <h2 className="text-xl font-semibold">{selectedNft.nickname}</h2> */}
-    //     <div style={{ textAlign: "center" }}>
-    //       <img
-    //         src={selectedNft.image}
-    //         alt={selectedNft.nickname}
-    //         className="my-4 w-96 mb-4"
-    //         style={{ maxWidth: "60%", display: "inline-block" }}
-    //       />
-    //     </div>
-
-    //     <hr className="border-t border-gray-300 my-4"></hr>
-    //     {/* 여기에 카드에 관한 추가 정보를 표시하세요 */}
-    // <div
-    //   style={{
-    //     color: "black",
-    //     display: "flex",
-    //     alignItems: "center",
-    //     marginLeft: "20px"
-    //   }}
-    // >
-    //   <TiMediaRecord style={{ marginRight: "0.5rem" }}></TiMediaRecord>
-    //   <h3>소유주: {selectedNft.nickname}</h3>
-    // </div>
-    // <div
-    //   style={{
-    //     color: "black",
-    //     display: "flex",
-    //     alignItems: "center",
-    //     marginLeft: "20px"
-    //   }}
-    // >
-    //   <TiMediaRecord style={{ marginRight: "0.5rem" }}></TiMediaRecord>
-    //   <p>동물 이름: {selectedNft.name}</p>
-    // </div>
-    // <div
-    //   style={{
-    //     color: "black",
-    //     display: "flex",
-    //     alignItems: "center",
-    //     marginLeft: "20px"
-    //   }}
-    // >
-    //   <TiMediaRecord style={{ marginRight: "0.5rem" }}></TiMediaRecord>
-    //   <p>나이: {selectedNft.age}</p>
-    // </div>
-    // <div
-    //   style={{
-    //     color: "black",
-    //     display: "flex",
-    //     alignItems: "center",
-    //     marginLeft: "20px"
-    //   }}
-    // >
-    //   <TiMediaRecord style={{ marginRight: "0.5rem" }}></TiMediaRecord>
-    //   <p>특징: {selectedNft.feature}</p>
-    // </div>
-    // <hr className="border-t border-gray-300 my-4"></hr>
-
-    //     <div className="mt-4 ml-64">
-    //       <Button
-    //         onClick={onClose}
-    //         variant="outlined"
-    //         color="red"
-    //         // className="mt-4 text-blue-500 hover:underline"
-    //       >
-    //         닫기
-    //       </Button>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
@@ -183,7 +101,6 @@ export function MidModal({ isOpen, onClose, selectedCard, goToNft }) {
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-      {/* <div className="modal-bg fixed top-0 left-0 w-full h-full bg-black opacity-50" /> */}
       <div
         className="modal-content bg-white p-4 rounded-lg shadow-md"
         style={{
@@ -193,9 +110,6 @@ export function MidModal({ isOpen, onClose, selectedCard, goToNft }) {
           margin: "0 auto"
         }}
       >
-        {/* <h2 className="text-xl font-semibold">{selectedNft.nickname}</h2> */}
-        {/* <img src={selectedCard.image} alt={selectedCard.nickname} className="my-4 max-w-full mb-4" /> */}
-        {/* 여기에 카드에 관한 추가 정보를 표시하세요 */}
         <div style={{ backgroundColor: "purple", padding: "65px" }}>
           <div style={{ color: "black" }}>
             <p>특징: {selectedCard.feature}</p>
