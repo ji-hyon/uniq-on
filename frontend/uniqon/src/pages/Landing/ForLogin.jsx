8fd1548d8917f4e9b96c3f23b4d1ae15a4bc3885
방상제 <sangje112@naver.com>
2023-10-05 13:59:48 +0900

    fix: 로그인 화면 오류 수정
    Change-Id: Ibce1fd3edd508b51ac59675a3378034db0800944

--- frontend/uniqon/src/pages/Landing/ForLogin.jsx
+++ frontend/uniqon/src/pages/Landing/ForLogin.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Scene from '../../components/Landing/Scene';
 import { CheckoutForm } from "../../components/Common/CheckoutForm";
 
 export function ForLogin() {
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  useEffect(() => {
+    setIsTransitioning(true);
+  }, []);
+
+  if (isTransitioning === false) {
+    // item 정보가 없을 경우 로딩 또는 오류 처리를 할 수 있습니다.
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
     <div className="w-screen h-screen p-0 m-0 overflow-hidden bg-black -webkit-touch-callout-none flex">
-      
+    
     <Scene />
+        
     </div>
     <div className="absolute top-[75px] right-[230px]">
     <CheckoutForm />
