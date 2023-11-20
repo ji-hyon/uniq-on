59c21fe4cdd7badf51f031db9610287ba54c4ed6
최지수 <cjjss11@naver.com>
2023-11-20 15:36:32 +0900

    docs: 주석 제거

--- frontend/uniqon/src/App.js
+++ frontend/uniqon/src/App.js
@@ -1,28 +1,26 @@
-import './App.css';
-import { Routes, Route, Link, useNavigate, Navigate } from 'react-router-dom';
-import { Transaction } from './pages/Transaction/Transaction';
-import { TranItemDetail } from './pages/Transaction/TranItemDetail';
-import { MyPage } from './pages/Mypage/MyPage';
-import { Wishlist } from './pages/Transaction/Wishlist';
-import { Landing } from './pages/Landing/Landing';
-import { NFT } from './pages/NFT/NFT';
-import { MainCollections } from './pages/Collections/MainCollections';
-import { MidCollections } from './pages/Collections/MidCollec';
-import { NFTList } from './pages/Collections/NFTList';
-import { Login } from './pages/Auth/Login';
-import { SignUp } from './pages/Auth/SignUp';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Transaction } from "./pages/Transaction/Transaction";
+import { TranItemDetail } from "./pages/Transaction/TranItemDetail";
+import { MyPage } from "./pages/Mypage/MyPage";
+import { Wishlist } from "./pages/Transaction/Wishlist";
+import { Landing } from "./pages/Landing/Landing";
+import { NFT } from "./pages/NFT/NFT";
+import { MainCollections } from "./pages/Collections/MainCollections";
+import { MidCollections } from "./pages/Collections/MidCollec";
+import { NFTList } from "./pages/Collections/NFTList";
+import { Login } from "./pages/Auth/Login";
+import { SignUp } from "./pages/Auth/SignUp";
 
-import { ForLogin } from './pages/Landing/ForLogin';
+import { ForLogin } from "./pages/Landing/ForLogin";
 
-import { Suspense } from 'react';
-import axios from 'axios';
-// import { useNavigate } from 'react-router-dom';
+import { Suspense } from "react";
+import axios from "axios";
 axios.interceptors.response.use(
   (response) => {
     return response;
   },
   (error) => {
-    // 여기서 401 오류를 처리할 수 있습니다.
     if (error.response.status === 401) {
       window.location.href = "/forlogin";
     }
@@ -33,23 +31,20 @@ function App() {
   return (
     <div className="App">
       <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/forlogin" element={<ForLogin />} />
-
-        <Route path="/transaction" element={<Transaction />} />
-        <Route path="/transaction/tranItemDetail/:id" element={<TranItemDetail />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/nft" element={<NFT />} />
-
-        <Route path="/collections" element={<MainCollections />} />
-        <Route path="/midcollections" element={<MidCollections />} />
-        <Route path="/nftlist" element={<NFTList />} />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/forlogin" element={<ForLogin />} />
+          <Route path="/transaction" element={<Transaction />} />
+          <Route path="/transaction/tranItemDetail/:id" element={<TranItemDetail />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/nft" element={<NFT />} />
+          <Route path="/collections" element={<MainCollections />} />
+          <Route path="/midcollections" element={<MidCollections />} />
+          <Route path="/nftlist" element={<NFTList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
       </Suspense>
     </div>
   );
